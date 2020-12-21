ca18683dfe0dc03417ef31cedae26d8c3e2602e9
Andrew Cole <andrew.cole@illallangi.com>
2020-12-21 17:28:58 +1100

    Added initial OIDC support

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,32 @@
-FROM caddy:2.2.1-builder AS builder
+FROM docker.io/library/golang:1.15.6 AS golang
+
+RUN apt-get -y update && apt-get install -y \
+      musl-tools
+
+RUN go get github.com/cloudflare/cfssl/cmd/...
+
+ENV CC=/usr/bin/musl-gcc
+RUN go build -ldflags "-linkmode external -extldflags -static" -o cfssl github.com/cloudflare/cfssl/cmd/cfssl
+RUN go build -ldflags "-linkmode external -extldflags -static" -o cfssljson github.com/cloudflare/cfssl/cmd/cfssljson
+
+
+FROM caddy:2.2.1-builder AS caddy
 
 RUN xcaddy build \
     --with github.com/greenpau/caddy-auth-portal \
     --with github.com/greenpau/caddy-auth-jwt \
     --with github.com/hairyhenderson/caddy-teapot-module@v0.0.3-0
 
+
 FROM caddy:2.2.1
 
 MAINTAINER Andrew Cole <andrew.cole@illallangi.com>
 
+COPY --from=caddy /usr/bin/caddy /usr/bin/caddy
 
-COPY --from=builder /usr/bin/caddy /usr/bin/caddy
+COPY --from=golang /go/cfssl /usr/local/bin/cfssl
+COPY --from=golang /go/cfssljson /usr/local/bin/cfssljson
+
 COPY contrib/confd-0.16.0-linux-amd64 /usr/local/bin/confd
 COPY contrib/dumb-init_1.2.2_amd64 /usr/local/bin/dumb-init
 COPY contrib/gosu_1.12_amd64 /usr/local/bin/gosu
@@ -24,6 +40,9 @@ RUN chmod +x \
         /usr/local/bin/gosu
 
 ENTRYPOINT ["/usr/local/bin/dumb-init", "--", "/entrypoint.sh"]
+ENV CADDY_FQDN=localhost
+ENV CADDY_CLIENT_SECRET=cb7b92fb-65ed-402e-b6e7-7c2f24754888
+ENV CADDY_TOKEN_SECRET=eae40c22-c718-4892-b631-77c68fc968a0
 
 ARG VCS_REF
 ARG VERSION
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,33 +1,48 @@
 #!/usr/bin/env sh
 
 UMASK_SET=${UMASK_SET:-022}
+CADDY_CONFIG=${CADDY_CONFIG:-/etc/caddy/Caddyfile}
 
-CONFD=$(which confd)
-GOSU=$(which gosu)
 CADDY=$(which caddy)
+CONFD=$(which confd)
+CFSSL=$(which cfssl)
+CFSSLJSON=$(which cfssljson)
 
-if [[ ! -x ${CONFD} ]]; then
-  echo "confd binary not found"
-  exit 1
-fi
-
-if [[ ! -x ${GOSU} ]]; then
-  echo "gosu binary not found"
-  exit 1
-fi
-
-if [[ ! -x $CADDY ]]; then
-  echo "caddy binary not found"
-  exit 1
-fi
-
-echo umask "$UMASK_SET"
-umask "$UMASK_SET" || exit 1
-
+echo "-----"
+echo "Generating Configuration Files"
 echo ${CONFD} -onetime -backend env -log-level debug
 ${CONFD} -onetime -backend env -log-level debug || exit 1
+echo "-----"
 
-CADDY_CONFIG=${CADDY_CONFIG:-/etc/caddy/Caddyfile}
+if [ -f /etc/caddy/tls.crt ]; then
+  echo "-----"
+  echo "Using supplied certificate"
+else
+  echo "Generating certificate"
+  echo "-----"  
+  
+  echo ${CFSSL} gencert -initca /etc/caddy/ca.json \| ${CFSSLJSON} -bare /etc/caddy/ca
+  ${CFSSL} gencert -initca /etc/caddy/ca.json | ${CFSSLJSON} -bare /etc/caddy/ca || exit 1
+  
+  echo ${CFSSL} gencert -ca /etc/caddy/ca.pem -ca-key /etc/caddy/ca-key.pem -config /etc/caddy/cfssl.json -profile=server /etc/caddy/tls.json \| ${CFSSLJSON} -bare /etc/caddy/tls
+  ${CFSSL} gencert -ca /etc/caddy/ca.pem -ca-key /etc/caddy/ca-key.pem -config /etc/caddy/cfssl.json -profile=server /etc/caddy/tls.json | ${CFSSLJSON} -bare /etc/caddy/tls || exit 1
+  
+  ln -s /etc/caddy/tls.pem /etc/caddy/tls.crt
+  ln -s /etc/caddy/tls-key.pem /etc/caddy/tls.key
+fi
+
+if [[ -x "/usr/local/bin/make-proxy-dirs.sh" ]]
+then
+  echo "Creating proxy and redirect directories"
+  /usr/local/bin/make-proxy-dirs.sh
+  echo "-----"
+fi
+
+echo "Checking Syntax"
+cat ${CADDY_CONFIG}
+echo ${CADDY} validate --config ${CADDY_CONFIG} --adapter caddyfile
+${CADDY} validate --config ${CADDY_CONFIG} --adapter caddyfile || exit 1
+echo "-----"
 
 echo ${*:-${CADDY} run --config ${CADDY_CONFIG} --adapter caddyfile}
 exec ${*:-${CADDY} run --config ${CADDY_CONFIG} --adapter caddyfile}
